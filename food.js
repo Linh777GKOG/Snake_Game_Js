7a36ca423d03f21d7db5f79260157ee78c6a731a
Linh Vu <linh777gkog@gmail.com>
2022-06-16 22:30:50 +0700

    expand randomfood

--- food.js
+++ food.js
@@ -10,3 +10,19 @@ export function update() {
     food = getRandomFoodPosition();
   }
 }
+
+export function draw(gameBoard) {
+  const foodElement = document.createElement('div');
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add('food');
+  gameBoard.appendChild(foodElement);
+}
+
+function getRandomFoodPosition() {
+  let newFoodPosition;
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition();
+  }
+  return newFoodPosition;
+}
