011692e280f26ed3d3e166afbab0ccd8ed89be5d
Linh Vu <linh777gkog@gmail.com>
2022-06-16 22:30:26 +0700

    make food snake

---
+++ food.js
@@ -0,0 +1,12 @@
+import { onSnake, expandSnake } from './snake.js';
+import { randomGridPosition } from './grid.js';
+
+let food = getRandomFoodPosition();
+const EXPANSION_RATE = 5;
+
+export function update() {
+  if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE);
+    food = getRandomFoodPosition();
+  }
+}
