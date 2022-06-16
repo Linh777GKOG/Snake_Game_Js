bd46d46c6f48f332cc5b21b23e37d5b7651434e5
Linh Vu <linh777gkog@gmail.com>
2022-06-16 22:28:11 +0700

    make snakebody

---
+++ snake.js
@@ -0,0 +1,17 @@
+import { getInputDirection } from './input.js';
+
+export const SNAKE_SPEED = 5;
+const snakeBody = [{ x: 11, y: 11 }];
+let newSegments = 0;
+
+export function update() {
+  addSegments();
+
+  const inputDirection = getInputDirection();
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
+}
